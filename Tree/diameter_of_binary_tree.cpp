5aafd054bed1716f996359ab33d7b408f0609e2a
Aditya Kumar Kasaudhan <akasaudhan02@gmail.com>
2024-01-11 03:21:16 +0530

    diameter of binary tree

---
+++ Tree/diameter_of_binary_tree.cpp
@@ -0,0 +1,34 @@
+/*
+Basically maximum length of path
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int depth(TreeNode* root, int &d){
+        if(root == NULL) return 0;
+
+        int l = depth(root->left, d);
+        int r = depth(root->right, d);
+        d = max(d, l+r);
+        return max(l,r)+1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root == NULL) return 0;
+        int d = 0;
+        depth(root, d);
+
+        
+
+        return d;
+    }
+};
